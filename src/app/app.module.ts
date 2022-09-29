d54e71c37749b08a213318c93f3237877521b950
ctninman <89664157+ctninman@users.noreply.github.com>
2022-09-29 09:25:27 -0400

    added dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
 import { UserComponent } from './user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core'
 @Component({
 	selector: 'app-user',
 	template: `
-		<p>Hello<p>
+		<input type="text" (input)="onUserInput($event)" [value]="name">
+    <!-- 2-way binding of the above statement -->
+    <!-- event binding and property binding together -->
+    <input type="text" [(ngModel)]="name" />
+		<p>Hello {{name}} <p>
 		<p>I'm the user</p>
 	`
 })
-export class UserComponent {}
+export class UserComponent {
+	name = 'CTNins'
+
+	onUserInput(event) {
+		this.name = event.target.value;
+
+	}
+}
